b639f886afe456743e4abddc706f0ba2b209032e
prana <prana@qtrust.id>
2024-11-26 09:25:53 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import MainLayout from "./layouts/MainLayout";
+import JobsPage from "./pages/JobsPage";
+import NotFoundPage from "./pages/NotFoundPage";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<Homepage />} />
+      <Route path="/jobs" element={<JobsPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Route>
+  )
+);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
---
+++ src/pages/JobsPage.jsx
@@ -0,0 +1,11 @@
+import Joblistings from "../components/JobListings";
+
+function JobPages() {
+  return (
+    <section className="bg-blue-50 px-4 py-6">
+      <Joblistings />
+    </section>
+  );
+}
+
+export default JobPages;
